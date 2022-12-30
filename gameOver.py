e980bf1000ba1723763ee25c9b3cbb8bf0fbb1f8
Cristian FLORUTA <florutacristian@gmail.com>
2022-12-30 13:07:05 +0200

    GameOver class created for informing the player

---
+++ gameOver.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+
+class GameOver(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+
+    def inform(self):
+        """
+        :return: Write "Game Over" on the game screen!
+        """
+        self.write(arg="GAME OVER!", align="center", font=("Courier", 24, "bold"), )
